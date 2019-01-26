77b82e51b0a8ba7b7a5fed86c8abbdcb09f1733c
Willis Plummer <willisplummer@gmail.com>
2019-01-26 12:05:42 -0500

    Updates

--- component---src-pages-confirm-tsx-9937de17c33c4b18eada.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{140:function(e,n,t){"use strict";t.r(n),t.d(n,"pageQuery",function(){return r});var a=t(0),o=t.n(a),i=t(148),r="844991288";n.default=function(e){var n=e.location,t=e.data.site.siteMetadata.title;return o.a.createElement(i.a,{location:n,title:t},o.a.createElement("h1",null,"Just one more thing..."),o.a.createElement("p",null,"Thank you for subscribing. You will need to check your inbox and confirm your subscription."))}}}]);
-//# sourceMappingURL=component---src-pages-confirm-tsx-9937de17c33c4b18eada.js.map
